966a86f3a53dafdb933ba81ac5d7f999543b6278
Dan Beaven <dm.beaven@gmail.com>
2020-04-06 11:47:25 +0100

    fix: fix function exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { Valid, Success, Failure, Proof, TypeOfProof, Check } from './types'
-export { is, outputString, isFailure, isSuccess, valid, failure, success } from './utils'
+export { is, outputString, isProven, check, failure, success } from './utils'
 
 import prove from './prove'
 export default prove
